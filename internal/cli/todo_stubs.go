95ed4108ac356fa4ded7718ade17055f1263ae38
Edwin Arley Upegui Vergara <60724138+edwinupegui@users.noreply.github.com>
2026-06-08 21:38:26 -0500

    feat(cli): todo rm + restore + purge with confirmation

--- internal/cli/todo_stubs.go
+++
@@ -1,7 +0,0 @@
-package cli
-
-import "github.com/spf13/cobra"
-
-func newTodoRmCmd() *cobra.Command     { return &cobra.Command{Use: "rm"} }
-func newTodoRestoreCmd() *cobra.Command { return &cobra.Command{Use: "restore"} }
-func newTodoPurgeCmd() *cobra.Command  { return &cobra.Command{Use: "purge"} }
